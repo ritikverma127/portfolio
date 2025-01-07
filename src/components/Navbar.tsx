d03c01290e78dfa6b4c13466eabcba52d28bf023
ritikverma127 <rv1338739@gmail.com>
2025-01-07 18:05:08 +0530

    again change the data

--- src/components/Navbar.tsx
+++ src/components/Navbar.tsx
@@ -20,7 +20,7 @@ const Header = () => {
               href="#home"
               className="text-sm hover:text-gray-600 transition-colors font-bold"
             >
-              Title
+              Home
             </Link>
             <Link
               href="#services"
